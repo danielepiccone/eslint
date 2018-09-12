7954993347ee6c9711343e51becc355cdc000b3b
Daniele Piccone <dpi@one.com>
2018-09-12 10:48:25 +0200

    Update: no-unmodified-loop-condition, object mutation with valueOf
    This commit:
    - Adds a (negative) test case for an object encapsulating a value
    and revealing it with valueOf().
    - Adds a (positive) test case for an object for the same object,
    revealing the value with a get() function.

--- tests/lib/rules/no-unmodified-loop-condition.js
+++ tests/lib/rules/no-unmodified-loop-condition.js
@@ -41,7 +41,9 @@ ruleTester.run("no-unmodified-loop-condition", rule, {
         "for (var foo = 0; foo;) { ++foo }",
         "var foo = 0, bar = 0; for (bar; foo;) { ++foo }",
         "var foo; if (foo) { }",
-        "var a = [1, 2, 3]; var len = a.length; for (var i = 0; i < len - 1; i++) {}"
+        "var a = [1, 2, 3]; var len = a.length; for (var i = 0; i < len - 1; i++) {}",
+        "var foo = { add: function () { this.value++ }, value: 0, get: function() { return this.value } }; while (foo.get() < 10) { foo.add() }",
+        "var foo = { add: function () { this.value++ }, value: 0, valueOf: function() { return this.value } }; while (foo < 10) { foo.add() }"
     ],
     invalid: [
         { code: "var foo = 0; while (foo) { } foo = 1;", errors: ["'foo' is not modified in this loop."] },
